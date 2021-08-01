5c11fad6662da49c0ede6801828c567e8938e9a5
blingo265 <33985241+blingo265@users.noreply.github.com>
2021-08-02 01:08:18 +0530

    Add files via upload
    Added method for caching a matrix inverse.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These are a pair of functions that cache the inverse of a matrix.
 
-## Write a short comment describing this function
+
+# This function takes a matrix as an input and returns a list of functions ( a "special vector") as an output.
+# The returned list of functions contains functions to set the matrix, get the matrix, set the value of matrix inverse,
+# and get the value of matrix inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  matinv <- NULL
+  set <- function(y) {
+    x <<- y
+    matinv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) matinv <<- inv
+  getinv <- function() matinv
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
-
+# This function accepts the list of functions (a "special vector") returned from makeCacheMatrix. It checks for cached value
+# for the inverse matrix. If a cached value is available, it returns the cached value as the matrix inverse. If a cached value is 
+# not available, then it calculates the matrix inverse, and returns the value.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  matinv <- x$getinv()
+  if(!is.null(matinv)) {
+    message("getting cached data")
+    return(matinv)
+  }
+  data <- x$get()
+  matinv <- solve(data, ...)
+  x$setinv(matinv)
+  matinv
 }
